4ceece6cce91d90d7e7c4810a815a24501cdf84e
saber <talhaoui.sabir@gmail.com>
2024-10-04 21:45:26 +0200

    update

--- src/pages/Images/Images.jsx
+++ src/pages/Images/Images.jsx
@@ -1,12 +1,12 @@
-import React, { useState } from "react";
+import  { useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
+import "./Images.css";
 
-import "./images.css";
 import {
   about_image,
   img1,
